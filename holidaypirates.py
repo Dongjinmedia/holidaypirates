4010f20397137ffc803bcfdf964216b4c8ef9bda
Dongjin Park <dongjinmedia@gmail.com>
2019-01-25 14:38:20 +0900

    Update holidaypirates.py

--- holidaypirates.py
+++ holidaypirates.py
@@ -1,17 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 
-def hello():
-	return 'Hello Raihan and holidaypirates!\n My name is Dongjin Park!'
+def index() :
+    return render_template('index.html')
 
-
-
-
-if __name__ == '__main__':
-
-	app.run(host="0.0.0.0", port="80")
+if (__name__ == '__main__') :
+    app.run(debug = 1, host = '0.0.0.0', port="80")
